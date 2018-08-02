3ae50ccb10704634df0315658aaa82e181bb1f72
JayJayAquino <36612928+JayJayAquino@users.noreply.github.com>
2018-08-01 18:13:02 -0700

    AutonScripts

---
+++ Proto1/include/autonScripts.h
@@ -0,0 +1,16 @@
+#ifndef AUTONSCRIPTS_H
+#define AUTONSCRIPTS_H
+
+#include "robot.h"
+
+void carbonRunRedPreload(robot bot);
+void carbonRunBluePreload(robot bot);
+void carbonJukeRunRedPreload(robot bot);
+void debug(robot bot);
+void wuRunRedPreload(robot bot);
+void wuRunBluePreload(robot bot);
+// void wuJukeRunRedPreload(robot bot);
+
+void runUart(void * parameter);
+
+#endif
