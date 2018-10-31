ea5855892e5c259225bbeb4a2311d3b8b6f52c8c
Pablolepz <plepe00@gmail.com>
2018-10-31 09:21:58 -0700

    Added basic PID

---
+++ PID_C_Ex_Driver.c
@@ -0,0 +1,11 @@
+#pragma config(Motor,  port2,           Mot1,        tmotorVex393_MC29, openLoop)
+
+#include "PID_C_Example.c"
+
+task main()
+{
+  while(true) //in a while true loop to maintain the angle desired
+  {
+
+  }
+}
---
+++ PID_C_Example.c
@@ -0,0 +1,37 @@
+
+//Not optimized for speed or memory, designed to show the system clearly
+
+float error = 0;
+float currSens = 0;
+float totalError = 0;
+float prevError = 0;
+float dt = 0.05; //arbitrary, but should be calculated if possible. Milliseconds
+
+float proportional_Cont(float input, float kp)
+{
+  return (error * kp);
+}
+float integral_Cont(float ki)
+{
+  totalError = totalError + (error * dt);
+  return (totalError * ki);
+}
+float derivative_Cont(float kd)
+{
+  return ((error - prevError)/(dt));
+}
+
+float PIDController(float target, float senseVal, float kp, float ki, float kd)
+{
+  float result;
+  /*target is your target value
+  sense is the value of a sensor
+  kp is your weight for the proportional controller
+  ki is your weight for the integral controller
+  kd is your weight for the derivative controller
+  */
+  currSens = senseVal;
+  error = input - currSens;
+  result = proportional_Cont(target, kp) + integral_Cont(ki) + derivative_Cont(kd)
+  return result;
+}
