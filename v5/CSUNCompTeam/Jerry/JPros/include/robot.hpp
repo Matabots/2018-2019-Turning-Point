333cfc91674221d11095aa9a5e8ebc350b1964d3
Pablo Lepe <plepe00@gmail.com>
2018-11-27 13:19:59 -0800

    reset
    .

---
+++ v5/CSUNCompTeam/Jerry/JPros/include/robot.hpp
@@ -0,0 +1,22 @@
+#ifndef ROBOT_HPP
+#define ROBOT_HPP
+
+#include "main.h"
+
+extern Controller master;
+extern Motor right_motor1;
+extern Motor right_motor2;
+extern Motor right_motor3;
+extern Motor left_motor1;
+extern Motor left_motor2;
+extern Motor left_motor3;
+extern Motor intake_motor;
+extern Motor flywheel_motor1;
+extern Motor flywheel_motor2;
+extern Motor Lift_motor1;
+extern Motor Lift_motor2;
+extern Motor endEff_motor;
+
+extern void setup_bot();
+
+#endif
