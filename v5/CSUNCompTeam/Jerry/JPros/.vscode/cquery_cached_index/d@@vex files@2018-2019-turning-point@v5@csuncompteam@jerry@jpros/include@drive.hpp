1df8a31d1c7b7d08e0a2a95c45f080c79cc3585d
Turtlewax32 <42014368+Turtlewax32@users.noreply.github.com>
2018-12-01 16:24:21 -0800

    update

---
+++ v5/CSUNCompTeam/Jerry/JPros/.vscode/cquery_cached_index/d@@vex files@2018-2019-turning-point@v5@csuncompteam@jerry@jpros/include@drive.hpp
@@ -0,0 +1,12 @@
+
+#ifndef DRIVE_CPP
+#define DRIVE_CPP
+#include "main.h"
+
+extern void screen_placeholder();
+extern void left_motors_go(int rpm);
+extern void right_motors_go(int rpm);
+extern void move_Chassis();
+extern void InchtoDeg(int inch); 
+
+#endif
